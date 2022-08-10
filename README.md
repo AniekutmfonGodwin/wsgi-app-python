b92a26238f6d058022ee969fbfb3b0834895e6c8
AniekutmfonGodwin <adriano8263@gmail.com>
2022-08-10 11:27:41 +0100

    done with setup

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 ### How to create a pure wsgi app with python in-built wsgiref module
 
+There are 2 ways of running the app instance or app function 
+
+* using `make_server` directly
+
+        try:
+            from wsgiref.simple_server import make_server
+            httpd = make_server("",3333,app)
+            print("serving on port 3333...")
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            
+            print("Goodbye!")
+
+* using `make_server` with context
+
+        with make_server("",3333,app) as httpd:
+            httpd.serve_forever()
+            print("server running at port 3333...")
+
--- main.py
+++ main.py
@@ -1,6 +1,87 @@
+from wsgiref.simple_server import make_server
 
+
+PORT = 3333
+
+
+
+class Application(object):
+    """creating wsgi app using class
+
+    Args:
+        object (_type_): _description_
+    """
+    def __call__(self,environ,response:callable):
+        
+        PATH_INFO = environ["PATH_INFO"]
+
+
+        if PATH_INFO == "/hello":
+            return self.hello
+
+        
+
+
+        status = "200 OK"
+        headers = [
+            ("Content-Type","text/html")
+        ]
+        response(
+            status,
+            headers
+        )
+
+        response_byte = "Hello, World !".encode()
+
+        return [response_byte]
+
+    def hello(self,environ,response):
+        status = "200 OK"
+        headers = [
+            ("Content-Type","text/html")
+        ]
+        response(
+            status,
+            headers
+        )
+
+        response_byte = "Hello, World !".encode()
+
+        return [response_byte]
+
+
+    def goodbye(self,environ,response):
+        status = "200 OK"
+        headers = [
+            ("Content-Type","text/html")
+        ]
+        response(
+            status,
+            headers
+        )
+
+        response_byte = "Good bye!".encode()
+
+        return [response_byte]
+
+    def notfound(self,environ,response):
+        status = '404 Not Found'
+        headers = [
+            ("Content-Type","text/html")
+        ]
+        response(
+            status,
+            headers
+        )
+
+        response_byte = "Not found".encode()
+
+        return [response_byte]
+
+    
 
 def app(environ,response:callable):
+    # print("environ ",environ)
     status = "200 OK"
     headers = [
         ("Content-Type","text/html")
@@ -14,17 +95,22 @@ def app(environ,response:callable):
 
     return [response_byte]
 
+start_message = "server running on port {port}\nvisit link to view pages\n\n http://localhost:{port}\n\n".format(port=PORT)
 
 if __name__ == "__main__":
     """
     you can run the server by using this to method
     """
-    try:
-        from wsgiref.simple_server import make_server
-        httpd = make_server("",3333,app)
-        print("serving on port 3333...")
-        httpd.serve_forever()
-    except KeyboardInterrupt:
+    # try:
+    #     httpd = make_server("",PORT,app)
+    #     print(start_message)
+    #     httpd.serve_forever()
+    # except KeyboardInterrupt:
+    #     print("Goodbye!")
         
-        print("Goodbye!")
+    
+    with make_server("",PORT,Application()) as httpd:
+        print(start_message)
+        httpd.serve_forever()
+    
     
